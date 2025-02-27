be9e3ed648d59399079f412ac9bde9453e173d9a
hwannow <vnfdlvgid061@naver.com>
2025-02-27 14:18:52 +0900

    feat: 웹 전체 기능 구축

--- js/index.js
+++ js/index.js
@@ -17,3 +17,45 @@ console.log(
 );
 
 console.log("알맞은 스크립트를 작성하세요");
+
+document.addEventListener("DOMContentLoaded", function () {
+  const commentInput = document.querySelector(".comment-input");
+  const submitButton = document.querySelector(".submit-button");
+  const cancelButton = document.querySelector(".cancel-button");
+  const commentList = document.querySelector(".comment-list");
+
+  // 등록 버튼 클릭 시 댓글 추가
+  submitButton.addEventListener("click", function () {
+      const commentText = commentInput.value.trim();
+
+      if (commentText === "") {
+          alert("댓글을 입력해주세요!");
+          return;
+      }
+
+      // 새로운 댓글 요소 생성
+      const newComment = document.createElement("li");
+      newComment.innerHTML = `
+          <div class="comment-item">
+            <div class="comment-author">
+              <img src="./images/comment-author-icon.png" alt="사용자 프로필 이미지" />
+              <span>방문자</span>
+            </div>
+            <div class="comment-content">
+              ${commentText}
+            </div>
+          </div>
+      `;
+
+      // 댓글 리스트에 추가
+      commentList.appendChild(newComment);
+
+      // 입력창 초기화
+      commentInput.value = "";
+  });
+
+  // 취소 버튼 클릭 시 입력창 비우기
+  cancelButton.addEventListener("click", function () {
+      commentInput.value = "";
+  });
+});
